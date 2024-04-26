da1acb567618d3ef05840a8488d66ce76c660599
jmm-1987 <jonym1987@gmail.com>
2024-04-26 12:11:02 +0200

    26/04 base de datos y form con diseño

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session, request, redirect, url_for
 import db
 from models import Tarea
+from datetime import date, datetime
 
 
 app = Flask(__name__)
@@ -8,11 +9,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    tareas_hoy = db.session.query(Tarea).filter(Tarea.fecha_alerta == date.today()).all()
+    print (tareas_hoy)
     return render_template("index.html")
 
 @app.route('/form_nueva_tarea')
 def formulario_tarea():
     return render_template("form_nueva_tarea.html")
+
+@app.route('/grabar_tarea', methods=["POST"])
+def grabar_tarea():
+    tarea = Tarea(titulo=request.form["titulo"],
+              contenido=request.form["contenido"],
+              fecha_alta = date.today(),
+              fecha_alerta=datetime.strptime(request.form["fecha_alerta"], '%Y-%m-%d'),
+              realizada = False)
+
+    db.session.add(tarea)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
