bf39c49520fc34e7cc6ebc33090aca20a7cccce4
jmm-1987 <jonym1987@gmail.com>
2024-04-26 11:45:58 +0200

    26/04 base de datos y form con diseño

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template, session, request, redirect, url_for
+import db
+from models import Tarea
 
-
-# Press Mayús+F1pip search everywhere for classes, files, tool windows, actions, and settings.
 
 app = Flask(__name__)
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+@app.route('/form_nueva_tarea')
+def formulario_tarea():
+    return render_template("form_nueva_tarea.html")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    db.Base.metadata.create_all(db.engine)
     app.run(debug=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
